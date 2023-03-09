07f794ecb74ee03b0aaa98713ff2ed61f759b811
morteza <mgahangir2006@gmail.com>
2023-03-09 15:44:09 +0330

    Initialize React App - Install TailWindCss-ext Forms,React with icons, routers

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+// import Home from './pages/Home';
+// import ForgotPassword from './pages/ForgotPassword';
+// import Offers from './pages/Offers';
+// import Profile from './pages/Profile';
+// import Signin from './pages/Signin';
+// import Signup from './pages/Signup';
+// import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <h1 className='flex items-center py-4 px-10'>Hello world</h1>
+      {/* <Router>
+        <Header />
+        <Routes>
+          <Route  path='/' element={<Home />}/>
+          <Route  path='/profile' element={<Profile />}/>
+          <Route  path='/sign-in' element={<Signin />}/>
+          <Route  path='/sign-up' element={<Signup />}/>
+          <Route  path='/forgot-password' element={<ForgotPassword />}/>
+          <Route  path='/offers' element={<Offers />}/>
+        </Routes>
+      </Router> */}
+    </>
+  );
+}
+
+export default App;
+ 
