22a93293c9ee83f377af9b3871498394bf7dac55
morteza <mgahangir2006@gmail.com>
2023-04-01 16:35:17 +0430

    Create the create listing page UI

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+import Signup from './pages/Signup';  
 import PrivateRoute from './components/PrivateRoute';
 import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import CreateListing from './pages/CreateListing';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route  path='/sign-up' element={<Signup />}/>
           <Route  path='/forgot-password' element={<ForgotPassword />}/>
           <Route  path='/offers' element={<Offers />}/>
+          <Route  path='/create-listing' element={<CreateListing />}/>
         </Routes>
       </Router>
       <ToastContainer
